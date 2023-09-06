661fc341110a6923121a7acf538c3b32aeba18de
Kiizuha <kiizuha@gnuweeb.org>
2023-09-06 12:25:21 +0000

    Update NekoPoi client version

--- nekopoi/client.py
+++ nekopoi/client.py
@@ -8,7 +8,7 @@ class NekoPoi(Methods):
     NEKOPOI_URL: str = "https://nekopoi.care"
     BASE_URL: str = "https://cu8auck2lc.3z094n2681i06q8k14w31cu4q80d5p.com/330cceade91a6a9cd30fb8042222ed56/71b8acf33b508c7543592acd9d9eb70d"
     HEADERS = {
-        "appbuildcode": "25030",
+        "appbuildcode": "25032",
         "appsignature": "pOplm8IDEDGXN55IaYohQ8CzJFvWsfXyhGvwPRD9kWgzYSRuuvAOPfsE0AJbHVbAJyWGsGCNUIuQLJ7HbMbuFLMWwDgHNwxOrYMH",
         "token": "XbGSFkQsJYbFC6pcUMCFL4oNHULvHU7WdDAXYgpmqYlh7p5ZCQ4QZ13GDgowiOGvAejz9X5H6DYvEQBMrc3A17SO3qwLwVkbn6YY",
         "user-agent": "okhttp/4.9.0"
